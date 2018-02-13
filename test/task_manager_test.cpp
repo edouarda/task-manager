4f9541f9da704ed6688cb56d97a12688ce51115f
Edouard A <edouard@quasardb.net>
2018-02-13 23:19:36 +0100

    Better test

--- test/task_manager_test.cpp
+++ test/task_manager_test.cpp
@@ -2,40 +2,52 @@
 
 #include <tsk/scheduler.hpp>
 
-BOOST_AUTO_TEST_CASE(run_simple_task)
+struct scheduler_fixture
+{
+    scheduler_fixture()
+    {
+        scheduler.start();
+    }
+
+    ~scheduler_fixture()
+    {
+        scheduler.stop();
+    }
+
+    template <typename T>
+    void wait_for_it(T & var)
+    {
+        static constexpr size_t max_count = 10'000;
+
+        size_t i = 0;
+
+        for (; !var && (i < max_count); ++i)
+        {
+            std::this_thread::sleep_for(std::chrono::milliseconds{1});
+        }
+
+        BOOST_TEST(i < max_count);
+    }
+
+    tsk::scheduler scheduler{2};
+};
+
+BOOST_FIXTURE_TEST_CASE(run_simple_task, scheduler_fixture)
 {
     volatile bool stop = false;
 
-    tsk::scheduler s{2};
+    scheduler.run([&stop]() { stop = true; });
 
-    s.start();
-
-    s.run([&stop]() { stop = true; });
-
-    while (!stop)
-    {
-        std::this_thread::sleep_for(std::chrono::milliseconds{1});
-    }
-
-    s.stop();
+    wait_for_it(stop);
 }
 
-BOOST_AUTO_TEST_CASE(one_parameter_)
+BOOST_FIXTURE_TEST_CASE(one_parameter, scheduler_fixture)
 {
     volatile int val = 0;
 
-    tsk::scheduler s{2};
+    scheduler.run([&val](int val1, int val2) { val = val1 + val2; }, 31330, 7);
 
-    s.start();
-
-    s.run([&val](int val1, int val2) { val = val1 + val2; }, 31330, 7);
-
-    while (!val)
-    {
-        std::this_thread::sleep_for(std::chrono::milliseconds{1});
-    }
+    wait_for_it(val);
 
     BOOST_TEST(val == 31337);
-
-    s.stop();
 }
